18f8fa94538871af955e7644407c7b22ed6448df
Rohit <rohitc@nycstudents.net>
2022-11-29 12:31:56 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        while (!StdIn.isEmpty()) {
-            Poker test = new Poker();
-            if (test.flush()) {
-                StdOut.println("Flush: " + test);
-            }
-            if (test.fullHouse()) {
-                StdOut.println("Full House: " + test);
-            }
-        }
+        Deck deck = new Deck();
+        Hand myHand = new Hand(deck);
+        myHand.display();
     }
 }
